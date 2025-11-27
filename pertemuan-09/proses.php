1312500f7bd33fd1888927a9dbea459414b85954
Satria Akbar <2511500043@mahasiswa.atmaluhur.ac.id>
2025-11-27 11:53:11 +0700

    menambahkan

--- pertemuan-09/proses.php
+++ pertemuan-09/proses.php
@@ -7,17 +7,19 @@ $_SESSION["sesnama"] = $sesnama;
 $_SESSION["sesemail"] = $sesemail;
 $_SESSION["sespesan"] = $sespesan;
 
-$arrBiodata = []
-"Nim" => $_POST["txtNim"] ?? ""
-"nama"=> $_POST["txtNmLengkap"] ?? ""
-"tempat" => $_POST["txtT4Lhr"] ?? ""
-"tanggal" => $_POST["txtTglLhr"] ?? ""
-"hobi" => $_POST["txtHobi"] ?? ""
-"pasangan" => $_POST["txtPasangan"] ?? ""
-"pekerjaan" => $_POST["txtKerja"] ?? ""
-"ortu"=> $_POST["txtNmOrtu"] ??""
-$txtNmKakak = $_POST["txtNmKakak"];
-$txtNmAdik = $_POST["txtNmAdik"];
+$arrBiodata = [
+"nim" => $_POST["txtNim"] ?? "",
+"nama"=> $_POST["txtNmLengkap"] ?? "",
+"tempat" => $_POST["txtT4Lhr"] ?? "",
+"tanggal" => $_POST["txtTglLhr"] ?? "",
+"hobi" => $_POST["txtHobi"] ?? "",
+"pasangan" => $_POST["txtPasangan"] ?? "",
+"pekerjaan" => $_POST["txtKerja"] ?? "",
+"ortu"=> $_POST["txtNmOrtu"] ?? "",
+"kakak"=> $_POST["txtNmKakak"] ?? "",
+"adik"=> $_POST["txtNmAdik"] ?? "",
+];
+
 
 $_SESSION["txtNim"] = $txtNim;
 $_SESSION["txtNmLengkap"] = $txtNmLengkap;
