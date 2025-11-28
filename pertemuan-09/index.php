53d28ad883d5db75089ebda965cee27f8de13c6c
Satria Akbar <2511500043@mahasiswa.atmaluhur.ac.id>
2025-11-28 20:51:14 +0700

    simpan file index.php

--- pertemuan-09/index.php
+++ pertemuan-09/index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+
 require_once __DIR__ . '/fungsi.php';
+
 $sesnama = "";
 if (isset($_SESSION["sesnama"])):
   $sesnama = $_SESSION["sesnama"];
@@ -122,7 +124,7 @@ $fieldConfig = [
 
     <section id="about">
       <h2>Tentang Saya</h2>
-      <?= tampilkanBiodata(conf: $fieldConfig, arr: $biodata) ?>      
+      <?= tampilkanBiodata( $fieldConfig, arr: $biodata) ?>      
     </section>
 
     <section id="contact">
